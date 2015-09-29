6a8965b4f4c2ff54521c9a22c1e1b4bec5e2e43e
Sveder <m@sveder.com>
2015-09-30 00:07:02 +0300

    Fixed a bug with unicode errord prints preventing feed item parsing.

--- script.py
+++ script.py
@@ -28,6 +28,12 @@ def get_timestamp( utc_date ):
    return timestamp
 
 
+def _print(msg):
+   try:
+      print msg
+   except: pass
+
+
 def parse_outline( o ):
    "Parses an OPML outline element and pushes the RSS feeds to Pocket"
    global sql_feeds_count, sql_new_count, json_failed_items, json_failed_feeds, sql_status_string
@@ -38,9 +44,9 @@ def parse_outline( o ):
          # Parse and push to Pocket
          f = feedparser.parse(o.xmlUrl)
          if hasattr(f.feed, "title"):
-            print(Fore.CYAN + Style.BRIGHT + "[INFO] Processing feed: " + Style.RESET_ALL + f.feed.title + Style.DIM + " [" + o.xmlUrl + "]" + Style.RESET_ALL)
+            _print(Fore.CYAN + Style.BRIGHT + "[INFO] Processing feed: " + Style.RESET_ALL + f.feed.title + Style.DIM + " [" + o.xmlUrl + "]" + Style.RESET_ALL)
          else:
-            print(Fore.CYAN + Style.BRIGHT + "[INFO] Processing feed: " + Style.RESET_ALL + Style.DIM + o.xmlUrl + Style.RESET_ALL)
+            _print(Fore.CYAN + Style.BRIGHT + "[INFO] Processing feed: " + Style.RESET_ALL + Style.DIM + o.xmlUrl + Style.RESET_ALL)
 
          for i in f.entries:
             try:
@@ -56,34 +62,34 @@ def parse_outline( o ):
                   raise Exception("No timestamp supplied by feed.")
 
                if ( latest_timestamp > config.min_time and latest_timestamp > last_check ): # Added since last scan
-                  print(Fore.CYAN + Style.BRIGHT + "[INFO] Submitting item to pocket: " + Style.RESET_ALL + i.title + Style.DIM + " [" + i.link + "]" + Style.RESET_ALL);
+                  _print(Fore.CYAN + Style.BRIGHT + "[INFO] Submitting item to pocket: " + Style.RESET_ALL + i.title + Style.DIM + " [" + i.link + "]" + Style.RESET_ALL);
 
                   try:
                      p.add(i.link, title=i.title, time=latest_timestamp)
                      sql_new_count += 1
                   except timeouted_pocket.RateLimitException:
-                     print(Fore.RED + Style.BRIGHT + "[ERROR] Rate limit exceeded. " + Style.RESET_ALL + Style.DIM + "https://getpocket.com/developer/docs/rate-limits" + Style.RESET_ALL)
+                     _print(Fore.RED + Style.BRIGHT + "[ERROR] Rate limit exceeded. " + Style.RESET_ALL + Style.DIM + "https://getpocket.com/developer/docs/rate-limits" + Style.RESET_ALL)
                      sql_status_string = "rate limit exceeded"
                      write_database()
                      exit(4)
                   except KeyboardInterrupt:
-                     print(Fore.YELLOW + Style.BRIGHT + "[WARNING] Interrupted by user." + Style.RESET_ALL)
+                     _print(Fore.YELLOW + Style.BRIGHT + "[WARNING] Interrupted by user." + Style.RESET_ALL)
                      sql_status_string = "interrupted"
                      write_database()
                      exit(130)
                   except:
                      if hasattr(i, "title"):
-                        print(Fore.YELLOW + Style.BRIGHT + "[WARNING] Could not post element to pocket: " + Style.RESET_ALL + Style.DIM + i.title)
+                        _print(Fore.YELLOW + Style.BRIGHT + "[WARNING] Could not post element to pocket: " + Style.RESET_ALL + Style.DIM + i.title)
                      else:
-                        print(Fore.YELLOW + Style.BRIGHT + "[WARNING] Could not post element to pocket:" + Style.RESET_ALL)
-                        print("  " + Style.DIM + str(i))
-                     print("  " + str(sys.exc_info()[0]))
-                     print("  -> " + str(sys.exc_info()[1]) + Style.RESET_ALL)
+                        _print(Fore.YELLOW + Style.BRIGHT + "[WARNING] Could not post element to pocket:" + Style.RESET_ALL)
+                        _print("  " + Style.DIM + str(i))
+                     _print("  " + str(sys.exc_info()[0]))
+                     _print("  -> " + str(sys.exc_info()[1]) + Style.RESET_ALL)
                      sql_status_string = "done with errors"
                      json_failed_items += jsonpickle.encode(i, unpicklable=False, make_refs=False) + ","
 
             except KeyboardInterrupt:
-               print(Fore.CYAN + Style.BRIGHT + "[INFO] Interrupted by user." + Style.RESET_ALL)
+               _print(Fore.CYAN + Style.BRIGHT + "[INFO] Interrupted by user." + Style.RESET_ALL)
                sql_status_string = "interrupted"
                write_database()
                exit(130)
@@ -93,19 +99,19 @@ def parse_outline( o ):
 
             except:
                if hasattr(i, "title"):
-                  print(Fore.YELLOW + Style.BRIGHT + "[WARNING] Invalid element: " + Style.RESET_ALL + Style.DIM + i.title)
+                  _print(Fore.YELLOW + Style.BRIGHT + "[WARNING] Invalid element: " + Style.RESET_ALL + Style.DIM + i.title)
                else:
-                  print(Fore.YELLOW + Style.BRIGHT + "[WARNING] Invalid element:" + Style.RESET_ALL)
-                  print("  " + Style.DIM + str(i))
-               print("  " + str(sys.exc_info()[0]))
-               print("  -> " + str(sys.exc_info()[1]) + Style.RESET_ALL)
+                  _print(Fore.YELLOW + Style.BRIGHT + "[WARNING] Invalid element:" + Style.RESET_ALL)
+                  _print("  " + Style.DIM + str(i))
+               _print("  " + str(sys.exc_info()[0]))
+               _print("  -> " + str(sys.exc_info()[1]) + Style.RESET_ALL)
                sql_status_string = "done with errors"
                json_failed_items += jsonpickle.encode(i, unpicklable=False, make_refs=False) + ","
 
          sql_feeds_count += 1
 
       except KeyboardInterrupt:
-         print(Fore.CYAN + Style.BRIGHT + "[INFO] Interrupted by user." + Style.RESET_ALL)
+         _print(Fore.CYAN + Style.BRIGHT + "[INFO] Interrupted by user." + Style.RESET_ALL)
          sql_status_string = "interrupted"
          write_database()
          exit(130)
@@ -114,16 +120,16 @@ def parse_outline( o ):
          raise
 
       except:
-         print(Fore.YELLOW + Style.BRIGHT + "[WARNING] Could not process feed: " + Style.RESET_ALL + Style.DIM + o.xmlUrl + Style.RESET_ALL)
-         print("  " + Style.DIM + str(sys.exc_info()[0]))
-         print("  -> " + str(sys.exc_info()[1]) + Style.RESET_ALL)
+         _print(Fore.YELLOW + Style.BRIGHT + "[WARNING] Could not process feed: " + Style.RESET_ALL + Style.DIM + o.xmlUrl + Style.RESET_ALL)
+         _print("  " + Style.DIM + str(sys.exc_info()[0]))
+         _print("  -> " + str(sys.exc_info()[1]) + Style.RESET_ALL)
          sql_status_string = "done with errors"
          json_failed_feeds += jsonpickle.encode({"url": o.xmlUrl, "feed": f.feed}, unpicklable=False, make_refs=False) + ","
    elif len(o) > 0: # Element has children
       for i in o:
          parse_outline(i)
    else: # Element does not have an RSS URL or children
-      print(Fore.CYAN + Style.BRIGHT + "[INFO] OPML Element is empty: " + Style.RESET_ALL + Style.DIM + str(o) + Style.RESET_ALL)
+      _print(Fore.CYAN + Style.BRIGHT + "[INFO] OPML Element is empty: " + Style.RESET_ALL + Style.DIM + str(o) + Style.RESET_ALL)
 
 # Database helpers
 def write_database():
@@ -136,11 +142,11 @@ def write_database():
        dbc.executemany("INSERT INTO log (scan_start_time, scan_end_time, new_count, feeds_count, status_string, status_extras) VALUES(?, ?, ?, ?, ?, ?)", [(sql_scan_start_time, sql_scan_end_time, sql_new_count, sql_feeds_count, sql_status_string, sql_status_extras)])
        db.commit()
        db.close()
-       print(Fore.CYAN + Style.BRIGHT + "[INFO] Database written." + Style.RESET_ALL)
+       _print(Fore.CYAN + Style.BRIGHT + "[INFO] Database written." + Style.RESET_ALL)
     except:
-       print(Fore.RED + Style.BRIGHT + "[ERROR] Could not insert log entry to database!"  + Style.RESET_ALL)
-       print("  " + Style.DIM + str(sys.exc_info()[0]))
-       print("  -> " + str(sys.exc_info()[1])  + Style.RESET_ALL)
+       _print(Fore.RED + Style.BRIGHT + "[ERROR] Could not insert log entry to database!"  + Style.RESET_ALL)
+       _print("  " + Style.DIM + str(sys.exc_info()[0]))
+       _print("  -> " + str(sys.exc_info()[1])  + Style.RESET_ALL)
        exit(2)
 
 sql_scan_start_time = get_timestamp(time.gmtime(time.time()))
@@ -170,9 +176,9 @@ try:
 
 
 except:
-   print(Fore.RED + Style.BRIGHT + "[ERROR] Could not connect to SQLite 3 database!" + Style.RESET_ALL)
-   print("  " + Style.DIM + str(sys.exc_info()[0]))
-   print("  -> " + str(sys.exc_info()[1]) + Style.RESET_ALL)
+   _print(Fore.RED + Style.BRIGHT + "[ERROR] Could not connect to SQLite 3 database!" + Style.RESET_ALL)
+   _print("  " + Style.DIM + str(sys.exc_info()[0]))
+   _print("  -> " + str(sys.exc_info()[1]) + Style.RESET_ALL)
    sql_status_string = "failed"
    write_database()
    exit(2)
@@ -185,9 +191,9 @@ try:
    outline = opml.parse(config.opml_path)
 
 except:
-   print(Fore.RED + Style.BRIGHT + "[ERROR] Could not parse OPML file!" + Style.RESET_ALL)
-   print("  " + Style.DIM + str(sys.exc_info()[0]))
-   print("  -> " + str(sys.exc_info()[1]) + Style.RESET_ALL)
+   _print(Fore.RED + Style.BRIGHT + "[ERROR] Could not parse OPML file!" + Style.RESET_ALL)
+   _print("  " + Style.DIM + str(sys.exc_info()[0]))
+   _print("  -> " + str(sys.exc_info()[1]) + Style.RESET_ALL)
    sql_status_string = "failed"
    write_database()
    exit(1)
